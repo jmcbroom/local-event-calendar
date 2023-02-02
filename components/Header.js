48a272b29732f646f6246c536207705c52a5c22c
Jimmy McBroom <james.mcbroom@gmail.com>
2023-02-02 15:08:28 -0500

    changes

--- components/Header.js
+++ components/Header.js
@@ -19,29 +19,39 @@ const Header = ({ nav, showNav, siteTitle, description }) => {
 
   return (
     <header>
-      <div className="container">
-        <Link href="/" passHref>
-          <h1 className="md:w-1/3 font-bold">{siteTitle}</h1>
-        </Link>
+      <Link href="/" passHref>
+        <h1>{siteTitle}</h1>
+      </Link>
 
-        <nav>
-          <ul>
-            <Link href={`/events`} passHref>
-              <li className={"/events" === router.pathname ? "nav-here bg-event bg-opacity-50" : "bg-event bg-opacity-50"}>
-                events
-              </li>
-            </Link>
-            <Link href={`/venues`} passHref>
-              <li className={"/venues" === router.pathname ? "nav-here bg-venue bg-opacity-50" : "bg-venue bg-opacity-50"}>
-                venues
-              </li>
-            </Link>
-          </ul>
-          <div onClick={() => showNav(true)}>
-            <FontAwesomeIcon icon={faBars} className="block md:hidden h-6 hover:cursor-pointer" />
-          </div>
-        </nav>
-      </div>
+      <nav>
+        <ul>
+          <Link href={`/events`} passHref>
+            <li
+              className={
+                "/events" === router.pathname
+                  ? "nav-here bg-event bg-opacity-50"
+                  : "bg-event bg-opacity-50"
+              }
+            >
+              events
+            </li>
+          </Link>
+          <Link href={`/venues`} passHref>
+            <li
+              className={
+                "/venues" === router.pathname
+                  ? "nav-here bg-venue bg-opacity-50"
+                  : "bg-venue bg-opacity-50"
+              }
+            >
+              venues
+            </li>
+          </Link>
+        </ul>
+        <div onClick={() => showNav(true)}>
+          <FontAwesomeIcon icon={faBars} className="block md:hidden h-6 hover:cursor-pointer" />
+        </div>
+      </nav>
     </header>
   );
 };
